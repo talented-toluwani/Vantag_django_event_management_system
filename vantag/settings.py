d19e5c4ef8c7b65e3b0b3c529add9b1213dd5760
Toluwani Edgal <edgaltoluwani@gmail.com>
2026-05-16 19:52:37 +0100

    Linked the apps to the installed apps

--- vantag/settings.py
+++ vantag/settings.py
@@ -22,6 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-!=x&=(z(4a4xbt_p4nravzg_dx$5-jk0p8u0w9=%+1$9s@r5o^'
 
+
+STATIC_URL = "/static/"
+STATIC_DIRS = [BASE_DIR / "static"]
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -37,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'events',
+    'accounts'
 ]
 
 MIDDLEWARE = [
